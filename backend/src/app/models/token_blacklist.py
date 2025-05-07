221f314de66d9661b044c029c0dfe664b8600f14
Nam Nguyen <ducnam1719@gmail.com>
2025-05-07 11:27:07 +0700

    feat: init project as the initial not using the boleiplate cuz hard to sclae

--- backend/src/app/models/token_blacklist.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String, DateTime
-from app.core.db.database import Base
-
-class TokenBlacklist(Base):
-    __tablename__ = "token_blacklist"
-    id = Column(Integer, primary_key=True)
-    token = Column(String, nullable=False, unique=True, index=True)
-    expires_at = Column(DateTime, nullable=False)
